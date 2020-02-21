f5f4901369c5040e785b16fa3995adcdac274d8e
alyssa_bartuch <alyssabartuch@gmail.com>
2020-02-21 00:40:38 -0600

    event handeling and and into to state

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -14,10 +14,10 @@ class StorePicker extends React.Component {
         // 1. stop the form from submitting 
         event.preventDefault();
         // 2. get the text from the input
-        console.log(this.myInput);
-        
+        const storeName = this.myInput.current.value;
         // 3. change the page to '/store/input-text'
-    }
+        this.props.history.push(`/store/${storeName}`);
+    };
 
     render() {
         return (
